c95675862826ff158618c104256f8952d901dc0a
Fedcomp <977989+Fedcomp@users.noreply.github.com>
2024-03-18 02:50:04 +0500

    Basic error handling

--- pass/src/lib.rs
+++ pass/src/lib.rs
@@ -1,19 +1,21 @@
 pub mod models;
 pub mod schema;
 
+use anyhow::Context;
 use diesel::prelude::*;
 use diesel_async::{RunQueryDsl, AsyncPgConnection};
 use self::models::{NewPassword, Password};
 
-pub async fn create_password(conn: &mut AsyncPgConnection, name: &str, value: &str) -> Password {
+pub async fn create_password(conn: &mut AsyncPgConnection, name: &str, value: &str) -> anyhow::Result<Password> {
     use crate::schema::passwords;
 
     let new_password = NewPassword { name, value };
-
-    diesel::insert_into(passwords::table)
+    let result = diesel::insert_into(passwords::table)
         .values(&new_password)
         .returning(Password::as_returning())
         .get_result(conn)
         .await
-        .expect("Error saving new password")
+        .with_context(|| format!("Failed to create new password"))?;
+
+    Ok(result)
 }
--- pass/src/main.rs
+++ pass/src/main.rs
@@ -1,24 +1,30 @@
+use std::env;
+use anyhow::Context;
 use diesel::prelude::*;
 use diesel_async::{RunQueryDsl, AsyncConnection, AsyncPgConnection};
-use std::env;
 use controllable_pass::schema::passwords;
 use controllable_pass::models::Password;
 use controllable_pass::create_password;
 
 #[tokio::main]
-async fn main() {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let mut conn = AsyncPgConnection::establish(&database_url).await.expect("Failed to connect");
+async fn main() -> anyhow::Result<()> {
+    let database_url = env::var("DATABASE_URL")
+        .with_context(|| format!("please set DATABASE_URL environment variable"))?;
+    let mut conn = AsyncPgConnection::establish(&database_url)
+        .await
+        .with_context(|| format!("Failed to connect to database. Please make sure DATABASE_URL is correct.") )?;
 
     let passwords: Vec<Password> = passwords::table
         .select(Password::as_select())
         .load(&mut conn)
         .await
-        .expect("Failed to query for passwords");
+        .with_context(|| format!("Failed to query for passwords"))?;
 
     if passwords.len() == 0 {
-        create_password(&mut conn, "example", "example").await;
+        create_password(&mut conn, "example", "example").await?;
     }
 
     dbg!(passwords);
+
+    Ok(())
 }
